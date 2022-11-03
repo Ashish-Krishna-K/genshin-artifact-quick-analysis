8660f95b11e29325b9dde102f5f4d382c008b85e
Ashish-Krishna-K <ashishk.krishna@gmail.com>
2022-11-03 22:14:01 +0530

    Finishes the logic; styling pending.

---
+++ index.js
@@ -0,0 +1,38 @@
+const piece = document.querySelector('#piece');
+const type = document.querySelector('#type');
+const btn = document.querySelector('button');
+const displayResult = document.querySelector('#display-result')
+
+const results = {
+    keep: 'Keep this artifact',
+    trash: 'Destroy this artifact'
+}
+
+btn.addEventListener('click', function(){
+    let inputPiece = piece.value;
+    let inputType = type.value;
+
+    const result = determineWorth(inputPiece, inputType);
+
+    renderResult(result);
+});
+
+
+function determineWorth(piece, type) {
+
+    if (type === 'crits') {
+        return results.keep;
+    }
+    if (piece === 'flower' || piece === 'feather') {
+        if (type > 0) return results.trash;
+    } else {
+        if (type > 1) return results.trash;
+    }
+    return results.keep;
+}
+
+function renderResult(result) {
+    displayResult.textContent = result;
+}
+
+
